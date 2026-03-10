eeb17462072da11e485108a92f76f9efb6addf97
devendra jadhav <devendrajadhav2470@gmail.com>
2026-03-10 18:04:43 +0530

    fix: UserService wrong import

--- backend/app/api/auth.py
+++ backend/app/api/auth.py
@@ -3,8 +3,7 @@ import logging
 from flask import request
 from flask import jsonify 
 from email_validator import validate_email,EmailNotValidError
-from app.models import User
-from app.services import UserService
+from app.services.user_service import UserService
 from functools import wraps
 
 logger = logging.getLogger(__name__)
@@ -29,15 +28,15 @@ def token_required(f):
 
 @auth_bp.route("/signup",methods=['POST'])
 def register_user():
-    data= request.get_data()
+    data= request.get_json()
 
     if not data or not data.get("email") or not data.get("password"):
-        return jsonify({"error": "Email and Password are required for registering"}), 404
+        return jsonify({"error": "Email and Password are required for registering"}), 401
 
     # email validation 
     email = data.get("email").strip()
     if not email:
-        return jsonify({"error": "Email cannot be empty"}), 404
+        return jsonify({"error": "Email cannot be empty"}), 401
     
     try:
         valid = validate_email(email)
@@ -45,28 +44,29 @@ def register_user():
         logger.info(f"Email validated and normalized: {normalized_email}")
     except EmailNotValidError as e:
         logger.info(f"Email validation failure: {e}")
-        return jsonify({"error": "Email validation failed"}), 404
+        return jsonify({"error": "Email validation failed"}), 401
 
     # password validation
     # for now it can be any string of 6 letters
     password = data.get("password")
     if len(password)!=6:
-        return jsonify({"error": "password length is not equal to 6 chars"}), 404
+        return jsonify({"error": "password length is not equal to 6 chars"}), 401
     
     user_service = UserService()
     user_service.create_user(email=normalized_email,password=password)
+    return jsonify({"message": f"user created {email}"})
 
 @auth_bp.route("/signin",methods=['POST'])
 def login_user():
-    data= request.get_data()
+    data= request.get_json()
     if not data:
         logger.info(f"no request body")
-        return jsonify({"error": "you must provide email and password for logging in"}), 404
+        return jsonify({"error": "you must provide email and password for logging in"}), 401
     email = data.get("email")
     password = data.get("password")
 
     if not email or not password:
-        return jsonify({"error": "you must provide email and password for logging in"}), 404
+        return jsonify({"error": "you must provide email and password for logging in"}), 401
 
     user_service = UserService()
 
@@ -77,7 +77,7 @@ def login_user():
         })
         return jsonify({"token": token})
     
-    return jsonify({"error": "user verification failed"}), 404
+    return jsonify({"error": "user verification failed"}), 401
 
         
 
